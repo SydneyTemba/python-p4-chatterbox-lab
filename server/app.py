30cf41f1668c468dbdd9e9ee1a8c9de7ff678d54
Temba Sydney Gabriel <tembasydney@gmail.com>
2024-07-14 21:30:48 +0300

    lab done

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 from models import db, Message
 
-app = Flask(__name__)
+app = Flask(__name__)  # Correct __name__
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -14,13 +14,36 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        message_list = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc()).all()]
+        return make_response(jsonify(message_list), 200)
+    
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.json.get('body'),
+            username=request.json.get('username')
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(jsonify(new_message.to_dict()), 200)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
 
-if __name__ == '__main__':
+    if request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return make_response(f'Message {message.id} deleted successfully!', 200)
+    
+    elif request.method == 'PATCH':
+        for key in request.json:
+            setattr(message, key, request.json.get(key))
+        db.session.add(message)
+        db.session.commit()
+        return make_response(jsonify(message.to_dict()), 201)
+
+if __name__ == '__main__':  # Correct __name__
     app.run(port=5555)
